9086573d62411d677730eed238504555d99f9781
Ayy <ayy@Ayys-MacBook-Pro.local>
2023-06-11 14:08:51 +1000

    update coles endpoint

--- src/api-urls/coles.ts
+++ src/api-urls/coles.ts
@@ -7,7 +7,8 @@ export const storeLocationsURL = (
   numberOfLocations: number,
 ) =>
   `https://www.coles.com.au/api/bff/locations/search?longitude=${longitude}&latitude=${latitude}&distance=${distance}&numberOfLocations=${numberOfLocations}`
+// TODO: add a variable in .env for the url since this is updated
 export const productSearchURL = (searchTerm: string) =>
-  `https://www.coles.com.au/_next/data/20230316.01_v3.29.0/en/search.json?q=${searchTerm}`
+  `https://www.coles.com.au/_next/data/20230529.02_v3.38.0/en/search.json?q=${searchTerm}`
 export const productImageURL = (imageURI: string) =>
   `https://productimages.coles.com.au/productimages/${imageURI}`
